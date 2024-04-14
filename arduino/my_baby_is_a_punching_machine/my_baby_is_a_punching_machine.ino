dcafd45fa12041827300babbaee09dfa593a2ddf
Tommy <tommy@iamnotagoodartist.com>
2024-04-14 14:26:28 -0700

    Get and display title(s)

--- arduino/my_baby_is_a_punching_machine/my_baby_is_a_punching_machine.ino
+++ arduino/my_baby_is_a_punching_machine/my_baby_is_a_punching_machine.ino
@@ -1,9 +1,20 @@
 #include <Arduboy2.h>
 #include <ArduboyTones.h>
+#include <Tinyfont.h>
 #include "songs.h"
+
+#include "FlashStringHelper.h"
 
 Arduboy2 arduboy;
 ArduboyTones arduboyTones(arduboy.audio.enabled);
+Tinyfont tinyfont = Tinyfont(arduboy.sBuffer, WIDTH, HEIGHT);
+
+const int8_t SONGS_COUNT = 1;
+const char * const SONG_TITLES[] PROGMEM {
+  WASHING_YOUR_FACE_TITLE
+};
+
+int8_t songIndex = 0;
 
 void setup() {
   arduboy.beginDoFirst();
@@ -12,17 +23,34 @@ void setup() {
   arduboy.setFrameRate(15);
 }
 
+void drawDisplay() {
+  tinyfont.setCursor(1, 1);
+  tinyfont.print(readFlashStringPointer(&SONG_TITLES[songIndex]));
+}
+
+void handleButtonPresses() {
+  if (arduboy.justPressed(A_BUTTON)) {
+    arduboyTones.noTone();
+  } else if (arduboy.justPressed(B_BUTTON)) {
+    arduboyTones.tones(WASHING_YOUR_FACE_SCORE);
+  }
+
+  if (arduboy.justPressed(RIGHT_BUTTON)) {
+    songIndex = (songIndex + 1) % SONGS_COUNT;
+  }else if (arduboy.justPressed(LEFT_BUTTON)) {
+    songIndex = songIndex > 0 ? songIndex - 1 : (SONGS_COUNT - 1);
+  }
+}
+
 void loop() {
   if (!arduboy.nextFrame()) {
     return;
   }
 
   arduboy.pollButtons();
+  handleButtonPresses();
+
   arduboy.clear();
-
-  if (arduboy.justPressed(B_BUTTON)) {
-    arduboyTones.tones(WASHING_YOUR_FACE);
-  }
-
+  drawDisplay();
   arduboy.display();
 }
--- compile_songs.sh
+++ compile_songs.sh
@@ -12,28 +12,29 @@ songs_path="arduino/my_baby_is_a_punching_machine/songs.h"
 echo "#pragma once" > "${songs_path}"
 
 for filename in $PWD/midi/*.mid; do
+    echo "${filename}"
+
     # midi2tones has strict input expectations: no full path, no extension
     relative_path="$(basename $filename)"
-    stub="${relative_path%.*}"
-    input="midi/${stub}"
+    no_extension="${relative_path%.*}"
+    input="midi/${no_extension}"
     output_path="${input}.c"
 
-    const_name=$(echo "${stub}" | tr '[a-z]' '[A-Z]')
+    const_stub=$(echo "${no_extension}" | tr "[a-z]" "[A-Z]" | tr " " "_")
+    title=$(echo "${no_extension}" | tr "_" " ")
 
-    echo "INPUT:    ${input}"
-    echo "STUB:     ${stub}"
-    echo "CONST:    ${const_name}"
-    echo
+    echo " - Creating ${output_path}"
+    $midi2tones -o2 "${input}" >/dev/null
+    sed -i '' "s/score/${const_stub}_SCORE/g" "${output_path}"
 
-    $midi2tones -o2 "${input}"
-    sed -i '' "s/score/${const_name}/g" "${output_path}"
-    echo
-
-    echo "Appending ${output_path} code to ${songs_path}"
+    echo " - Appending to ${songs_path}"
     cat "${output_path}" | grep -v "^//" >> "${songs_path}"
+    echo "const char ${const_stub}_TITLE[] PROGMEM = \"${title}\";" >> "${songs_path}"
 
-    echo "Deleting ${output_path}"
+    echo " - Deleting ${output_path}"
     rm "${output_path}"
+
+    echo
 done
 
 }
