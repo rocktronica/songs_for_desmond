874ae1a74f31e092c5dd2d88e3be89cde8e60b9b
Tommy <tommy@iamnotagoodartist.com>
2024-08-04 14:58:27 -0700

    Guy in a Cop Car Running a Red Light

--- arduino/songs_for_desmond/tracks.h
+++ arduino/songs_for_desmond/tracks.h
@@ -2,6 +2,7 @@
 
 // Songs are songs; tracks are ordered songs
 const int8_t TRACKS[] = {
+  GUY_IN_A_COP_CAR_RUNNING_A_RED_LIGHT,
   WE_ARE_DRESSING_UP_AS_A_FAMILY_OF_SKELETONS,
   WHOS_MY_LITTLE_BABY_BIRD,
   ANY_KIND_OF_CAR_AND_ANY_KIND_OF_TRUCK,
